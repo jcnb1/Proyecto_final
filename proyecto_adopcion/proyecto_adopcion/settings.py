3b98fcf3353b84b383948ab7b1a4402763d9bffb
Juan Cruz Noe Bernachea <juancruznoebernachea@gmail.com>
2021-03-11 23:04:58 -0300

    mesagge(cambio 2.0)

--- proyecto_adopcion/proyecto_adopcion/settings.py
+++ proyecto_adopcion/proyecto_adopcion/settings.py
@@ -120,8 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static",'/Proyecto final/proyecto_adopcion/static/',]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
+MEDIA_URL = '/images/'
 
 
 
